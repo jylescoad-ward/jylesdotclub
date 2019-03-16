2b2abd1f86f358074c08b9f003571a300e16d90b
Jyles Coad-Ward <github@jylescoad-ward.com>
2019-03-16 15:19:39 +0800

    push announcement feature [really buggy]

--- vendor/js/announce.js
+++ vendor/js/announce.js
@@ -1,7 +1,40 @@
-var displayAnnouncement = "No";
-var announcementContent = `This is a test announcement. Please do not worry.`;
-var currentAnnouncementDate = `7th March 2019`;
+var announceJSON;
+var announceJSONURL = "announce.json";
+var announceJSONRequest = new XMLHttpRequest();
+
+var displayAnnouncement; var announcementContent; var currentAnnouncementDate;
 var announceID = document.getElementById("announce");
+function getAnnouncement() {
+	announceJSONRequest.onload = function() {}
+		announceJSONRequest.open('GET', announceJSONURL);
+		announceJSONRequest.responseType = 'json';
+		announceJSONRequest.send();
+		announceJSONRequest.onload = function() {
+		announceJSON = announceJSONRequest.response;
+	};
+	JSON.parse(announceJSON);
+	displayAnnouncement = announceJSON.show;
+	announcementContent = announceJSON.content;
+	currentAnnouncementDate = announceJSON.date;
+}
+getAnnouncement();
+
+
+function pushAnnouncement() {
+	var a = document.getElementById("announcementDate").value;
+	var b = document.getElementById("announcementDetails").value;
+	var c = document.getElementById("showDropdownBox").value;
+	var announceJSONToPush = {
+		"date": a,
+		"content": b,
+		"show": c
+	};
+	var fso = new ActiveXObject("Scripting.FileSystemObject");
+	var fh = fso.CreateTextFile("./announce.json", true);
+	fh.writeline(announceJSONToPush);
+	fh.Close();
+	
+}
 
 function wait(ms){
    var start = new Date().getTime();
@@ -19,8 +52,11 @@ function checkAnnouncements() {
 		announceID.innerHTML = `<strong>No Current Announcements</strong>`;
 	}
 	else{
-		announceID.innerHTML = `<strong style="font-size:14px;">New Announcement</strong><br>` + announcementContent + "<br><br>" + currentAnnouncementDate;
+		announceID.innerHTML = `<strong style="font-size:14px;">New Announcement</strong><br>` + announcementContent + `<br><br>` + currentAnnouncementDate;
 	}
 }
 
-checkAnnouncements()
+
+
+checkAnnouncements();
+
